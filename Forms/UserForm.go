4b6de46e54a44a8c0ad009a853f517a23c98aa70
R2D2Dan <Danil150799@gmail.com>
2022-04-06 18:50:43 +0300

    Добавил проверку пользователя

--- Forms/UserForm.go
+++ Forms/UserForm.go
@@ -1,7 +1,7 @@
 package www
 
 import (
-	_ "Authorization/DataBase"
+	database "Authorization/DataBase"
 	"log"
 	"net/http"
 )
@@ -14,7 +14,22 @@ func User(w http.ResponseWriter, r *http.Request) {
 	if user == "" && pass == "" {
 		log.Println("Нет данных")
 		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
 
+	if func(u, p string, db database.Data) bool {
+		for _, k := range db.Users {
+			if k.Login == u && k.Password == p {
+				return true
+			}
+		}
+		return false
+	}(user, pass, database.DB()) {
+		log.Println("Пользователь найден")
+	} else {
+		log.Println("Пользователь не найден")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
 
 }
